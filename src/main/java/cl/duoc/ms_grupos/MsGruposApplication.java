0fcd481785ca6080834dcfcc1845ffa1e20734b2
César Saavedra C. <ces.saavedra@duocuc.cl>
2026-05-09 17:08:51 -0400

    Cesar
    Primer Commit

---
+++ src/main/java/cl/duoc/ms_grupos/MsGruposApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_grupos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGruposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGruposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ms_grupos/MsGruposApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_grupos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGruposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
